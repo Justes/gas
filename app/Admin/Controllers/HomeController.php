761467fd6954d1c703fe4cfb7c010532cea48a49
xiaojun <liuyajun@zhidekan.me>
2021-01-30 17:42:41 +0800

    map:bug

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -5,34 +5,35 @@ namespace App\Admin\Controllers;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Layout\Content;
 use App\Models\{Company, Station};
+use App\Models\Zone;
 
 class HomeController extends Controller
 {
     public function index(Content $content)
     {
-
-        $stations   = Station::all(["id", "lat", "lng", "station_name", "type", "permit"])->toJson();
-        $companies  = Company::all(["id", "lat", "lng", "company_name"])->toJson();
         
-        return $content->view('admin.home', compact('stations', 'companies'));
-			/*
-            ->title('Dashboard')
-            ->description('Description...');
-            ->row(Dashboard::title())
-            ->row(function (Row $row) {
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
-            });
-			 */
+        $stations   = Station::all(["id", "lat", "lng", "station_name", "type", "permit", "company_id", "addr", "contact_user", "contact_user_mobile"])->toArray();
+        $companies  = Company::all(["id", "lat", "lng", "company_name", "addr", "belongs", "contact_user", "contact_user_mobile"]);
+        $counts = [
+            'station' => count($stations),
+            'company' => count($companies),
+        ];
+        if ($stations) {
+            foreach ($stations as $key => $value) {
+                $zones = Zone::whereRaw('FIND_IN_SET(?,station_ids)',[$value['id']])->where("zone_status", 0)->select(['zone_name', "zone_status"])->get()->toArray();
+                $str = '';
+                if ($zones) {
+                    foreach($zones as $item) {
+                        $str .= $item['zone_name'].', ';
+                    }
+                    $str = rtrim($str, ', ');
+                }
+                $stations[$key]['zones'] = $str;
+            }
+        }
+        
+        $stations = json_encode($stations);
+        $companies = $companies ->toJson();
+        return $content->view('admin.home', compact('stations', 'companies', 'counts'));
     }
 }
