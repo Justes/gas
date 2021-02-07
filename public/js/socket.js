a5e9deb3f46a0f19a67e0f6d92a5f52eb0878cd2
ouying <ouyingying@zhidekan.me>
2021-02-07 10:06:05 +0800

    更新聊天功能

--- public/js/socket.js
+++ public/js/socket.js
@@ -9,9 +9,11 @@ layui.use(['element', 'layer'], function(){
 
 var layim;
 var getlist = '/api/user/chat';
+var friendList = '/api/user/contacters';
 var socketUrl = "ws://8.129.161.138:8181/ws";
 var token = LA.user.token;
-
+var my_id = LA.user.id;
+var chatArr = [];
 // 获取用户信息
 $(function(){
 	$.ajax({
@@ -19,6 +21,7 @@ $(function(){
      	headers:{'token':token},
      	success: function(res) {
      		var data = res.data;
+            chatArr = data;
 			var mine = {
 				  "username": LA.user.username //我的昵称
 				  ,"id": LA.user.id //我的ID
@@ -29,11 +32,53 @@ $(function(){
 			// 初始化IM
 			initIM(mine)
      		$.each(data, function(index, item) {
-     			buildItem(item)     			
+                item.num = 0;
+     		    if(item.chat_type==1 && item.to==my_id){
+
+                }else {
+                    buildItem(item)
+                }
             })
         }
 	});
-	
+
+    $.ajax({
+        url: friendList,
+        headers:{'token':token},
+        success: function(res) {
+            var data = res.data;
+            var html = "";
+            $.each(data.rooms, function(index, item) {
+                html += '<li id="'+item.room_id+'" name="'+item.room_name+'" avatar="'+item.room_pic+'" chat_type="2"' ;
+                html += 'openid="'+item.to+'">';
+                html += '<img src="'+item.room_pic+'">';
+                html += '<span>'+item.room_name+'</span>';
+                html += '</li>';
+            });
+           $(".chatGroupList").append(html);
+
+            var list = "";
+            for(var i in data.users){
+                list +='<div class="f-item">'
+                list +='<div class="letter">'+i+'</div>'
+                list +='<ul class="friend-msg">'
+
+                var ulist = "";
+                $.each(data.users[i], function(index, item) {
+                    ulist +='<li id="'+item.user_id+'" name="'+item.name+'" avatar="'+item.avatar+'" chat_type="1" openid="'+item.user_id+'">'+
+                            '<img src="'+item.avatar+'">'+
+                            '<span>'+item.name+'</span>'+
+                            '</li>'
+                });
+                list += ulist;
+                list +='</ul>';
+                list +='</div>';
+            }
+            $(".friend-wrapper").append(list)
+        }
+    });
+
+
 	// 初始化即时通讯
 	function initIM(mine) {
 		layui.use('layim', function(layim){
@@ -70,20 +115,17 @@ $(function(){
 				} else{
 					var data = eval("("+res.data+")");
 				}
-				console.log(data);
-				if(data.type === 1){
-					var message = {
-					  username: data.name
-					  ,avatar: data.avatar
-					  ,id: data.user_id
-					  ,type: 'friend'
-					  ,friend: data.user_id
-					  ,content: data.msg
-					  ,timestamp: data.timestamp * 1000
-					};
-					//console.log(message);
-					layim.getMessage(message);
-				}
+                console.log('收到的消息'+JSON.stringify(data));
+				// console.log(chatArr)
+                chatArr.forEach(function (item,index) {
+                    if(data.oid == item.oid){
+                        item.num = item.num+1;
+                        item.msg = data.msg;
+                        item.msg_type = data.msg_type;
+                        updateItem(item,index)
+                    }
+                });
+                layim.getMessage(data);
 			};
 			//监听的错误
 			socket.onerror = function(e) {
@@ -93,80 +135,117 @@ $(function(){
 			
 			//监听发送消息
 			layim.on('sendMessage', function(res){
+			    console.log(res);
 				// 发送消息
-				var mine = res.mine;
-				// console.log(mine);
-				var to = res.to;
 				var data = {
 						"token": token,
 						"msgid":(new Date()).getTime().toString(),
 						"timestamp":Date.parse(new Date())/1000,
 						"type":1,
-						"chat_type": 1,
-						"msg_type": 1,
-						"user_id": mine.id,
-						"to":Number(to.id),
-						"msg":mine.content,
-						"name":mine.username,
-						"avatar":mine.avatar,
-						"file_url":'',
-						"file_name":'',
-						"file_ext":'',
+						"chat_type": Number(res.type),
+						"msg_type": Number(res.msg_type),
+						"user_id": my_id,
+						"to":Number(res.id),
+						"msg":res.msg,
+						"name":res.name,
+						"avatar":res.avatar,
+						"file_url":res.file_url,
+						"file_name":res.file_name,
+						"file_ext":res.file_ext,
 						"extras":'',
 					};
-				console.log(data);
 				socket.send( JSON.stringify(data) );
 			});
 			
 			
-	
-			$(".chatList").on('click', '.layim-list-item', function(){
+
+			//消息列表
+			$(".chatList").on('click', '.msg-item', function(){
 				var name = $(this).attr("name");
 				var avatar = $(this).attr("avatar");
 				var openid = $(this).attr("openid");
-				var type = $(this).attr("type");
+				var type = $(this).attr("chat_type");
+				var num = $(this).children().children('span');
+				num.html(0);
+				num.addClass('hide');
+				console.log(num);
 				layim.chat({
 				  name: name //名称
 				  ,type: type //聊天类型
+                  ,chat_type: type //聊天类型
 				  ,avatar: avatar //头像
 				  ,id: openid //好友id
 				  ,closeBtn:2
 				})
-			})
-			$(".chatGroupList").on('click', '.layim-list-item', function(){
-				var name = $(this).attr("name");
+			});
+			//通讯录-群
+			$(".chatGroupList").on('click', 'li', function(){
+                var name = $(this).attr("name");
 				var avatar = $(this).attr("avatar");
-				var unitid = $(this).attr("unitid");
+				var unitid = $(this).attr("id");
+				var type =  $(this).attr("chat_type");
 				layim.chat({
 				  name: name //名称
-				  ,type: 'group' //聊天类型
+				  ,type: type //聊天类型
+                    ,chat_type: type //聊天类型
 				  ,avatar: avatar //头像
 				  ,id: unitid //好友id
 				  ,closeBtn:2
 				})
 			})
-			
-			
+            //通讯录-人
+            $(".friend-wrapper").on('click', '.f-item ul li', function(){
+                var name = $(this).attr("name");
+                var avatar = $(this).attr("avatar");
+                var unitid = $(this).attr("id");
+                var type =  $(this).attr("chat_type");
+                layim.chat({
+                    name: name //名称
+                    ,type: type //聊天类型
+                    ,chat_type: type //聊天类型
+                    ,avatar: avatar //头像
+                    ,id: unitid //好友id
+                    ,closeBtn:2
+                })
+            })
 		})
 	}
-	
-	
-	function buildItem(item) {
-		var html = '<li class="layim-list-item" name="'+item.name+'" avatar="'+item.avatar+'"';
-		if(item.chat_type == 1){
-			html += ' openid="'+item.to+'" type="friend">';
-			html += '<img src="'+item.avatar+'">';
-			html += '<span>'+item.name+'</span>';
-			html += '</li>';
-			$(".chatList").append(html)
-        } else if(item.chat_type == 2) {
-        	html += ' unitid="'+item.to+'">';
-        	html += '<img src="'+item.avatar+'">';
-        	html += '<span>'+item.name+'</span>';
-			html += '</li>';
-			$(".chatGroupList").append(html)
+
+	function updateItem(item,index) {
+        var html = '<li class="msg-item" name="'+item.name+'" avatar="'+item.avatar+'" chat_type="'+item.chat_type+'"' ;
+        html += 'openid="'+item.to+'" type="friend">';
+        html += '<div class="img-box"><img src="'+item.avatar+'"><span class="'+(item.num<1?"hide":"")+'">'+item.num+'</span></div>';
+        html += '<div><h4>'+item.name+'</h4>';
+        if(item.msg_type==2){
+            html+='<p>[语音]</p>';
+        }else if(item.msg_type==3){
+            html+='<p>[图片]</p>';
+        }else if(item.msg_type==4){
+            html+='<p>[文件]</p>';
+        }else {
+            html+='<p>'+item.msg+'</p>';
         }
-	}
+        html += '</div></li>';
+        $(".chatList li").eq(index).remove();
+        $(".chatList").prepend(html);
+    }
+	function buildItem(item) {
+		var html = '<li class="msg-item" name="'+item.name+'" avatar="'+item.avatar+'" chat_type="'+item.chat_type+'"' ;
+        html += 'openid="'+item.to+'" type="friend">';
+        html += '<div class="img-box"><img src="'+item.avatar+'"><span class="'+(item.num<1?"hide":"")+'">'+item.num+'</span></div>';
+        html += '<div><h4>'+item.name+'</h4>';
+        if(item.msg_type==2){
+            html+='<p>[语音]</p>';
+        }else if(item.msg_type==3){
+            html+='<p>[图片]</p>';
+        }else if(item.msg_type==4){
+            html+='<p>[文件]</p>';
+        }else {
+            html+='<p>'+item.msg+'</p>';
+        }
+        html += '</div></li>';
+        $(".chatList").append(html)
+    }
 	$("#chat-aside-toggle").click(function(){
 		$('#aside-container').toggleClass('on')
 	})
