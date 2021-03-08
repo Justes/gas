7f57874baa0d7fe75c56440b81adea6ffd149703
Justes <2395781272@qq.com>
2021-03-08 10:41:00 +0800

    mod

--- vendor/laravel-admin-ext/latlong/resources/views/latlong.blade.php
+++ vendor/laravel-admin-ext/latlong/resources/views/latlong.blade.php
@@ -31,10 +31,10 @@ var companies 	= [];
 
         <div class="row">
             <div class="col-md-3">
-                <input id="{{$id['lng']}}" name="{{$name['lng']}}" class="form-control" value="{{ old($column['lng'], $value['lng']) }}" {!! $attributes !!} />
+                <input id="{{$id['lng']}}" name="{{$name['lng']}}" class="form-control" value="{{ old($column['lng'], $value['lng'] ?? null) }}" {!! $attributes !!} />
             </div>
             <div class="col-md-3">
-                <input id="{{$id['lat']}}" name="{{$name['lat']}}" class="form-control" value="{{ old($column['lat'], $value['lat']) }}" {!! $attributes !!} />
+                <input id="{{$id['lat']}}" name="{{$name['lat']}}" class="form-control" value="{{ old($column['lat'], $value['lat'] ?? null) }}" {!! $attributes !!} />
             </div>
 
             @if($provider != 'yandex')
